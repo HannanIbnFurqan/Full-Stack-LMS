2230de90b38351b07a9db2346c36a076ce33ef4d
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-24 23:51:58 +0530

    run file upload logic before register

--- routes/user.Route.js
+++ routes/user.Route.js
@@ -7,9 +7,11 @@ import { register } from '../controller/user.controller.js';
 import { login } from '../controller/user.controller.js';
 import { logout } from '../controller/user.controller.js';
 import { getUser } from '../controller/user.controller.js';
+// multer middleWare
+import upload from '../middleWare/multer.middleware.js';
 
 // all userRouter
-userRouter.post('/register', register)
+userRouter.post('/register',upload.single("avatar"), register)
 userRouter.post('/login', login)
 userRouter.get('/logout', logout)
 userRouter.get('/getUser', isLoggedIn, getUser)
