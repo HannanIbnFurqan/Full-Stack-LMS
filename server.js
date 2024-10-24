59d4a17ae696cf5f2c46e691e658af61f2e706c7
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-24 23:53:48 +0530

    cloudinary config

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import errorMiddleware from './middleWare/error.middleware.js';
 import userRoutes from './routes/user.Route.js'
 import connectDB from './config/dbConnection.js';
 import dotenv from 'dotenv'
+import cloudinary from 'cloudinary'
 const app = express()
 dotenv.config()
 // middleWare 
@@ -11,6 +12,12 @@ app.use(express.json());
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 5000
+// cloudinary Configuration
+cloudinary.v2.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret // Click 'View API Keys' above to copy your API secret
+});
 
 // user route
 app.use('/api/v1/user', userRoutes);
@@ -22,6 +29,6 @@ app.all('*',(req,res)=>{
 app.use(errorMiddleware);
 
 app.listen(PORT, ()=>{
-    console.log('server is running')
+    console.log('server is running',PORT)
     connectDB()
 })
