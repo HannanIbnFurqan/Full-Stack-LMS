e46fab604c4ed272efbda69a36c48c06d9d4e326
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-29 01:13:24 +0530

    add some npm package

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ import errorMiddleware from './middleWare/error.middleware.js';
 import userRoutes from './routes/user.Route.js'
 import connectDB from './config/dbConnection.js';
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+import cors from 'cors'
 import cloudinary from 'cloudinary'
 const app = express()
 dotenv.config()
 // middleWare 
-express.urlencoded()
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors())
 app.use(cookieParser())
+app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 5000
 
