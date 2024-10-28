fbe17f18882f955c56a00d5dea889e3d0bdd4b36
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-28 16:09:47 +0530

    update for security

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,14 +1,13 @@
 import nodemailer from 'nodemailer'
 
-
 const sendEmail = async(email, subject, message)=>{
 // Create transporter with your email service provider’s SMTP settings
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: process.env.host,
+    port: process.env.port,
     auth: {
-        user: 'mabel.rowe@ethereal.email',
-        pass: 'r4e8zYhFh8HbftBF7s'
+        user: process.env.user,
+        pass: process.env.pass
     }
 });
 
