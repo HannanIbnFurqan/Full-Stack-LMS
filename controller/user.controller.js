176e77c1904a4bd136b45dfcdd99d1a653507d6e
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-29 01:39:45 +0530

    create changePassword functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -191,4 +191,35 @@ const resetPassword = async (req,res,next)=>{
         message: 'password changed successfully'
     })
 }
-export {register,login,logout,getUser, forgotPassword, resetPassword}
+
+const changePassword = async(req,res)=>{
+    const {oldPassword, newPassword} = req.body;
+    const {id} = req.user;
+
+    if(!oldPassword || !newPassword){
+        return next(new AppError('All field are mandatory', 400));
+    }
+
+    const user = await User.findById(id).select('password');
+
+    if(!user){
+        return next(new AppError('User does not exist',400))
+    }
+
+    const isPasswordValid = await user.comparePassword(oldPassword);
+
+    if(!isPasswordValid){
+        return next(new AppError('Invalid old password',400))
+    }
+
+    user.password = newPassword
+    await user.save()
+
+    user.password = undefined;
+
+    res.status(200).json({
+        success: true,
+        message: 'password change successfully'
+    })
+}
+export {register,login,logout,getUser, forgotPassword, resetPassword, changePassword}
