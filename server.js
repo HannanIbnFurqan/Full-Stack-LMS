492b791f758cb3e418d527e156b474162f03b53b
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-31 00:23:24 +0530

    add new related course

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser'
 import  urlencoded  from 'express';
 import errorMiddleware from './middleWare/error.middleware.js';
 import userRoutes from './routes/user.Route.js'
+import courseRoute from './routes/course.Route.js';
 import connectDB from './config/dbConnection.js';
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser';
@@ -16,17 +17,20 @@ app.use(express.json());
 app.use(cors())
 app.use(cookieParser())
 app.use(bodyParser.json())
-
-const PORT = process.env.PORT || 5000
-
-// user route
-app.use('/api/v1/user', userRoutes);
 // cloudinary Configuration
 cloudinary.v2.config({ 
     cloud_name: process.env.cloud_name, 
     api_key: process.env.api_key, 
     api_secret: process.env.api_secret // Click 'View API Keys' above to copy your API secret
 });
+
+const PORT = process.env.PORT || 5000
+
+// user route
+app.use('/api/v1/user', userRoutes);
+// course route
+app.use('/api/v1/course', courseRoute);
+
 
 app.all('*',(req,res)=>{
     res.status(404).send('OOPS!! 404 page not found');
