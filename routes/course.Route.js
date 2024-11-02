87eb213d591b0d41e8799989d398ccc0aef2a893
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-11-02 11:28:06 +0530

    update route

--- routes/course.Route.js
+++ routes/course.Route.js
@@ -1,14 +1,32 @@
 import express from 'express'
 import {getAllCourses, getLecturesByCoureId, createCourse, updateCourse, removeCourse} from '../controller/course.controller.js'
-import { isLoggedIn } from '../middleWare/auth.middleware.js';
+import { authorizedRoles, isLoggedIn } from '../middleWare/auth.middleware.js';
 import upload from '../middleWare/multer.middleware.js';
 const courseRoute = express.Router();
 
-courseRoute.get('/', getAllCourses)
-courseRoute.post("/", upload.single('thumbnail'),createCourse)
+courseRoute.route.get('/', getAllCourses)
+ .post(
+    isLoggedIn, 
+    authorizedRoles('ADMIN'), 
+    upload.single('thumbnail'),
+    createCourse)
 
-courseRoute.get('/:id', isLoggedIn, getLecturesByCoureId)
-courseRoute.put('/:id', updateCourse)
-courseRoute.delete('/:id', removeCourse)
+courseRoute.route.get('/:id', isLoggedIn, getLecturesByCoureId)
+ .put(
+    isLoggedIn,
+     authorizedRoles('ADMIN'),
+    updateCourse)
+ .delete(
+    isLoggedIn,
+     authorizedRoles('ADMIN'),
+    removeCourse
+)
+.post(
+    isLoggedIn,
+    authorizedRoles('ADMIN'),
+    upload.single('lectures'),
+    addLectureToCourseById
+)
+
 
 export default courseRoute
