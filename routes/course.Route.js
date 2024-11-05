145e994d0c08aa30873274ec933be9f4c8894257
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-11-05 23:05:58 +0530

    fixed some bugs in course route

--- routes/course.Route.js
+++ routes/course.Route.js
@@ -1,32 +1,16 @@
 import express from 'express'
-import {getAllCourses, getLecturesByCoureId, createCourse, updateCourse, removeCourse} from '../controller/course.controller.js'
+import {getAllCourses, getLecturesByCoureId, createCourse, updateCourse, removeCourse, addLectureToCourseById} from '../controller/course.controller.js'
 import { authorizedRoles, isLoggedIn } from '../middleWare/auth.middleware.js';
 import upload from '../middleWare/multer.middleware.js';
 const courseRoute = express.Router();
 
-courseRoute.route.get('/', getAllCourses)
- .post(
-    isLoggedIn, 
-    authorizedRoles('ADMIN'), 
-    upload.single('thumbnail'),
-    createCourse)
+courseRoute.get('/', getAllCourses)
+courseRoute.post('/createCourse',isLoggedIn, authorizedRoles('ADMIN'), upload.single('thumbnail'),createCourse)
 
-courseRoute.route.get('/:id', isLoggedIn, getLecturesByCoureId)
- .put(
-    isLoggedIn,
-     authorizedRoles('ADMIN'),
-    updateCourse)
- .delete(
-    isLoggedIn,
-     authorizedRoles('ADMIN'),
-    removeCourse
-)
-.post(
-    isLoggedIn,
-    authorizedRoles('ADMIN'),
-    upload.single('lectures'),
-    addLectureToCourseById
-)
+courseRoute.get('/:id', isLoggedIn, getLecturesByCoureId)
+courseRoute.put('/updateCourse',isLoggedIn,authorizedRoles('ADMIN'),updateCourse)
+courseRoute.delete('/removeCourse',isLoggedIn,authorizedRoles('ADMIN'),removeCourse)
+courseRoute.post('/addLectureToCourseById',isLoggedIn,authorizedRoles('ADMIN'), upload.single('lectures'), addLectureToCourseById)
 
 
 export default courseRoute
