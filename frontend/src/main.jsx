b882266a0b919b1822b3041e33378a01733dc379
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-06 23:13:35 +0530

    setup import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import css
 import './index.css'
+// import 
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { createRoot } from 'react-dom/client'
+
+// import components
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-    <Toaster></Toaster>
+  <Toaster></Toaster>
   </BrowserRouter>,
 )
